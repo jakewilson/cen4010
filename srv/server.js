e92d2b08426e6f04b77b707ddba9b121dd8ebf53
Jake Wilson <jakewilsonfl@gmail.com>
2016-03-16 17:50:55 -0400

    Greatly enhances server page and adds www directory.

--- srv/server.js
+++ srv/server.js
@@ -1,45 +1,10 @@
 Server = (function() {
-    var http = require('http');
-    var fs   = require('fs');
-
-    var writeError = function() {
-        return '<h1>404</h1>Page not found';
-    };
-
-    /* writes the HTTP response header */
-    var writeHead = function(filename, response) {
-        fs.readFile(filename, (err, data) => {
-            if (err) { // file not found - issue 404
-                response.writeHead(404, {
-                    'Content-Type': 'text/html' });
-        
-                response.write(writeError());
-            } else {
-                response.writeHead(200, {
-                    'Content-Type': 'text/html',
-                    'Content-Length': data.length });
-        
-                response.write(data);
-            }
-            response.end();
-        });
-    };
-
+    var express = require('express');
+    var app = express();
     return {
-        /* starts the server */
         start: function(port) {
-            http.createServer((request, response) => {
-                if (request.method === 'GET') {
-                    var getFileName = function() {
-                        var filename = require('url').parse(request.url).pathname.substring(1); // remove leading '/'
-                
-                        return filename === '' ? 'index.html' : filename;
-                    };
-
-                    writeHead(getFileName(), response);
-                }
-            }).listen(port);
+            app.use(express.static('www'));
+            app.listen(port);
         }
     };
-
 });
