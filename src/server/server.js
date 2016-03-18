be9c1a455214f5f5e9a52c077759c756565144ac
Jake Wilson <jakewilsonfl@gmail.com>
2016-03-17 20:04:36 -0400

    Moves post handling code out of start function. Removes login.html page.

--- src/server/server.js
+++ src/server/server.js
@@ -5,43 +5,44 @@ var app = express();
 
 var urlParser = bodyParser.urlencoded({extended: false});
 
+app.post('/index.html', urlParser, (req, res, next) => {
+    if (!req.body)
+        return res.sendStatus(400);
+    var user = req.body.user;
+    var pass = req.body.pass;
+    var msg  = 'Invalid request.';
+
+    if (!user || !pass) {
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.write('You must enter a username and a password');
+        res.end();
+    }
+
+    db.getPlayer(user, (err, row) => {
+        if (!err) {
+            if (row === undefined) {
+                msg = 'Username ' + user + ' does not exist.';
+            } else {
+                if (row.pass === pass) {
+                    res.redirect(302, 'game.html');
+                    return res.end();
+                } else {
+                    msg = 'Invalid password.';
+                }
+            }
+        } else {
+            console.log(err);
+        }
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.write(msg);
+        res.end();
+    });
+});
+
 module.exports = {
   start: function(port) {
     app.use(express.static('src/client/'));
     app.listen(port);
 
-    app.post('/index.html', urlParser, (req, res, next) => {
-        if (!req.body)
-            return res.sendStatus(400);
-        var user = req.body.user;
-        var pass = req.body.pass;
-        var msg  = 'Invalid request.';
-
-        if (!user || !pass) {
-            res.writeHead(400, {'Content-Type': 'text/html'});
-            res.write('You must enter a username and a password');
-            res.end();
-        }
-
-        db.getPlayer(user, (err, row) => {
-            if (!err) {
-                if (row === undefined) {
-                    msg = 'Username ' + user + ' does not exist.';
-                } else {
-                    if (row.pass === pass) {
-                        res.redirect(302, 'index.html');
-                        return res.end();
-                    } else {
-                        msg = 'Invalid password.';
-                    }
-                }
-            } else {
-                console.log(err);
-            }
-            res.writeHead(400, {'Content-Type': 'text/html'});
-            res.write(msg);
-            res.end();
-        });
-    });
   }
 }
