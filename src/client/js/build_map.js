b626904b7adfa9b5e36a1da7a631b0ab5ac1d98e
Jake Wilson <jakewilsonfl@gmail.com>
2016-03-26 15:19:17 -0400

    Changes the way animations are done.

--- src/client/js/build_map.js
+++ src/client/js/build_map.js
@@ -3,10 +3,11 @@ var game = new Phaser.Game(800, 16 * 15, Phaser.AUTO, 'phaser-example', { preloa
 function preload() {
   game.load.tilemap('map', './assets/tilemap/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
   game.load.image('tiles', './assets/tiles/super_mario.png');
-  game.load.atlasJSONHash('protagonist-walk', './assets/sprites/protagonist/walk.png', './assets/sprites/protagonist/walk.json');
+
+  game.load.atlasJSONHash('protagonist', './assets/spritesheets/protagonist.png', './assets/spritesheets/protagonist.json');
 }
 
-var map, cursors, layer, player, frameSpeed;
+var map, cursors, layer, player, frameSpeed, playerSpeed;
 
 function create() {
   map = game.add.tilemap('map');
@@ -17,8 +18,10 @@ function create() {
 
   map.setCollisionBetween(14, 16, true, layer);
 
-  player = game.add.sprite(0, 16 * 9, 'protagonist-walk');
-  player.animations.add('walk');
+  player = game.add.sprite(0, 16 * 9, 'protagonist');
+  player.frameName = 'walk1.png';
+
+  player.animations.add('walk', ['walk2.png', 'walk3.png', 'walk4.png', 'walk1.png']); // TODO change walk animation to be only one step at a time
 
   frameSpeed = 5;
   playerSpeed = 4;
@@ -42,11 +45,11 @@ function update() {
     player.x -= 4;
   }
   if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    player.play('walk', frameSpeed);
+    player.play('jump', frameSpeed);
     player.y -= 4;
   }
   if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    player.play('walk', frameSpeed);
+    player.play('jump', frameSpeed);
     player.y += 4;
   }
 }
