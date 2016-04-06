e44d81e03faa1d2a2bfd4d48056aa7a49fa96942
Jake Wilson <jakewilsonfl@gmail.com>
2016-04-06 10:42:53 -0400

    Increases the Player's score upon releasing an animal.

--- src/client/game/Animal.js
+++ src/client/game/Animal.js
@@ -1,13 +1,21 @@
 var Animal = function(game) {
   TileSpriteGroup.call(this, game, 'animal');
   this._scoreAmt = 100;
+  this._released = false;
 }
 
 /** Animal inherits TileSpriteGroup */
 Animal.prototype = Object.create(TileSpriteGroup.prototype);
 Animal.prototype.constructor = Animal;
 
-Animal.prototype._onOverlap = function(player, animal) {
-  // play the animation and kill the sprite on completion
-  animal.animations.play('anim', 3, false, true);
+Animal.prototype.setCollision = function(player) {
+  var onOverlap = function(player_sprite, animal) {
+    if (!this._released) {
+      this._released = true;
+      // play the animation and kill the sprite on completion
+      animal.animations.play('anim', 3, false, true);
+      player.updateScore(this._scoreAmt);
+    }
+  };
+  TileSpriteGroup.prototype.setCollision.call(this, player, onOverlap);
 }
