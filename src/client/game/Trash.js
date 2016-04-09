a5366c8df97413daa9c05c0b00be94a20bbc400f
Jake Wilson <jakewilsonfl@gmail.com>
2016-04-09 19:29:23 -0400

    Fixes small syntax error.

--- src/client/game/Trash.js
+++ src/client/game/Trash.js
@@ -16,6 +16,7 @@ Trash.prototype.create = function(spriteObjects, startingFrameIdx) {
 
 Trash.prototype.setCollision = function(player) {
   var onOverlap = function(player_sprite, Trash) {
-    }
+    // TODO
+  };
   TileSpriteGroup.prototype.setCollision.call(this, player, onOverlap);
 }
