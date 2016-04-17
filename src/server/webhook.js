47fc457fb7e46639d1a999f6a196415390db3445
Jonathan Frias <jonathan.frias1@gmail.com>
2016-04-17 17:02:32 -0400

    use bash, and shit!

--- src/server/webhook.js
+++ src/server/webhook.js
@@ -2,12 +2,12 @@ var express = require('express');
 var db = require('./db');
 var bodyParser = require('body-parser');
 var app = express();
-var bash = require('child_process').exec;
+var sh = require('child_process').exec;
 var urlParser = bodyParser.urlencoded({extended: false});
 
 app.post('/', urlParser, (req, res, next) => {
-  bash("git pull -f origin deploy");
-  bash("sleep 5; grunt");
+  sh("git pull -f origin deploy");
+  sh("bash -c 'grunt'");
   res.sendStatus(200);
   console.log("Pull triggered at " + new Date());
   res.end();
