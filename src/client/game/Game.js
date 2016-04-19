8ec8d14d6bd252d18cd9819c8f34d2ed189e495e
Nicholas Hecht <nicholas_hecht@yahoo.com>
2016-04-19 19:11:04 -0400

    Adds death screen state to Game

--- src/client/game/Game.js
+++ src/client/game/Game.js
@@ -8,7 +8,10 @@ var loadState = {
     game.load.atlasJSONHash('highScore', '../images/mainMenuInverted.png', '../images/mainMenuInverted.json');	
     game.load.atlasJSONHash('quit', '../images/mainMenuInverted.png', '../images/mainMenuInverted.json');	
     game.load.atlasJSONHash('play', '../images/mainMenuInverted.png', '../images/mainMenuInverted.json');	
-    game.load.image('background', '../images/MainMenu.png');	
+    game.load.atlasJSONHash('yesNo', '../images/yesNoButtons.png', '../images/yesNoButtons.json');	
+    game.load.image('background', '../images/mainMenu.png');	
+    game.load.image('death', '../images/gameOver.png');	
+    game.load.image('name', '../images/Meatpocalypse.png');	
   },
 
   create: function() {
@@ -17,9 +20,54 @@ var loadState = {
   }
 };
 
+var first = true;
+
 var mainMenu = {
   create: function() {
-    game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.5);
+    image = game.add.image(0, 0, 'background');
+    quit = game.add.button(675, 150, 'quit', quitClick, this, 'RQuit.png', 'WQuit.png', 'RQuit.png');
+    play_button = game.add.button(50, 160, 'play', playClick, this, 'RPlayButton.png', 'WPlay.png', 'RPlayButton.png');
+    highScore_button = game.add.button(250, 395, 'highScore', hsClick, this, 'WRHS.png', 'RWHS.png', 'WRHS.png');
+
+    function playClick() {
+      game.state.start('play');
+    }
+		
+    function hsClick() {
+      window.location = "/highScore.html";
+    }
+		
+    function quitClick() {
+      window.location = "/index.html";
+    }
+  }
+};
+
+var deathScreen = {
+  create: function() {
+    game.add.tileSprite(0, 0, 1024, 1024, 'background');
+    death = game.add.sprite(275, 50, 'death');
+    death.scale.set(0.5, 0.5);
+    yes_button = game.add.button(50, 160, 'yesNo', yesClick, this, 'Ryes.png', 'Wyes.png', 'Ryes.png');
+    no_button = game.add.button(675, 160, 'yesNo', noClick, this, 'Rno.png', 'Wno.png', 'Rno.png');
+    death = game.add.sprite(190, 580, 'name');
+    death.scale.set(1, 1);
+
+    function yesClick() {
+      //Write to the database here
+      game.state.start('play');
+    }
+		
+    function noClick() {
+      //Write to the database here
+      game.state.start('load');
+    }
+  }
+};
+
+var victoryScreen = {
+  create: function() {
+    game.add.image(game.world.centerX, game.world.centerY, 'death').anchor.set(0.5);
     quit = game.add.button(675, 150, 'quit', quitClick, this, 'RQuit.png', 'WQuit.png', 'RQuit.png');
     play_button = game.add.button(50, 160, 'play', actionOnClick, this, 'RPlayButton.png', 'WPlay.png', 'RPlayButton.png');
     highScore_button = game.add.button(250, 395, 'highScore', hsClick, this, 'WRHS.png', 'RWHS.png', 'WRHS.png');
@@ -91,4 +139,5 @@ function pauseFunction() {
 game.state.add('load', loadState, true);
 game.state.add('mainMenu', mainMenu);
 game.state.add('play', playState);
-
+game.state.add('deathScreen', deathScreen);
+game.state.add('victoryScreen', victoryScreen);
