8bd7b4ab99c5affede1f0fda8193f451e612d058
Jonathan Frias <jonathan.frias1@gmail.com>
2016-04-19 18:01:03 -0400

    Change to master

--- src/server/webhook.js
+++ src/server/webhook.js
@@ -6,7 +6,7 @@ var sh = require('child_process').exec;
 var urlParser = bodyParser.urlencoded({extended: false});
 
 app.post('/', urlParser, (req, res, next) => {
-  sh("git pull -f origin deploy");
+  sh("git pull -f origin master");
   sh("bash -c 'grunt'");
   res.sendStatus(200);
   console.log("Pull triggered at " + new Date());
